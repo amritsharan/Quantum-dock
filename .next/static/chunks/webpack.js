4f0d19ea7ccf9e60bdd8833c1646e32863f47a5e
1SI23CI005 AMRIT S R <1si23ci005@sit.ac.in>
2026-01-06 03:25:00 +0000

    Re-committing sign-up page fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef14c834c71c5d8a"; }
+/******/ 		__webpack_require__.h = function() { return "4df163e88453ac75"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
